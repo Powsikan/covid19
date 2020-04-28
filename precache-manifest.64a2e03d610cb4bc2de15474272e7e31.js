abcf9ad95774756599be4a7e7e91980460acba6e
powsokan <powsi1997@gmail.com>
2020-04-28 11:17:58 +0530

    Updates

---
+++ precache-manifest.64a2e03d610cb4bc2de15474272e7e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39f2fac3680094a64baa9c7dd57fe46",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "d2a1571ac758a599d9b1",
+    "url": "/covid19/static/css/main.d9462a75.chunk.css"
+  },
+  {
+    "revision": "b1bcc42eeda5db9d5b19",
+    "url": "/covid19/static/js/2.598147c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.598147c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a1571ac758a599d9b1",
+    "url": "/covid19/static/js/main.f6feb873.chunk.js"
+  },
+  {
+    "revision": "bb08661687ef9ed76af2",
+    "url": "/covid19/static/js/runtime-main.06ba182b.js"
+  }
+]);
